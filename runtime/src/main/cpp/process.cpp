a16385b6793cc9df56f14dc6700b6ed77fbb78b6
v7878 <46866705+vova7878@users.noreply.github.com>
2024-11-11 19:37:26 +0700

    test

--- runtime/src/main/cpp/process.cpp
+++ runtime/src/main/cpp/process.cpp
@@ -1,8 +1,12 @@
 #include "process.hpp"
 #include <string.h> // NOLINT(*-deprecated-headers)
 
+#include <android/log.h>
+
 void process_get_package_name(JNIEnv *env, jstring process_name, char **package_name) {
     const char *str = env->GetStringUTFChars(process_name, nullptr);
+
+    __android_log_print(ANDROID_LOG_WARN, "ZYGISK", "%s", str);
 
     *package_name = strdup(str);
 
