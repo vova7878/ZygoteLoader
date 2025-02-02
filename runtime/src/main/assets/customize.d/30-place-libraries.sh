3dac77f7b9213da46cd379b8d61162d50b62edd6
v7878 <46866705+vova7878@users.noreply.github.com>
2025-02-02 21:30:25 +0700

    Fix place-libraries script

--- runtime/src/main/assets/customize.d/30-place-libraries.sh
+++ runtime/src/main/assets/customize.d/30-place-libraries.sh
@@ -1,27 +1,27 @@
 # Place Zygisk libraries
 
-if [ "$ARCH" = "arm" ] then
+if [ "$ARCH" = "arm" ]; then
   ui_print "- Placing arm32 libraries"
 
   rm -rf "$MODPATH/zygisk/x86.so" "$MODPATH/zygisk/x86_64.so" "$MODPATH/zygisk/arm64-v8a.so"
   rm -rf "$MODPATH/lib/x86" "$MODPATH/lib/x86_64" "$MODPATH/lib/arm64-v8a"
 fi
 
-if [ "$ARCH" = "arm64" ] then
+if [ "$ARCH" = "arm64" ]; then
   ui_print "- Placing arm64 libraries"
 
   rm -rf "$MODPATH/zygisk/x86.so" "$MODPATH/zygisk/x86_64.so"
   rm -rf "$MODPATH/lib/x86" "$MODPATH/lib/x86_64"
 fi
 
-if [ "$ARCH" = "x86" ] then
+if [ "$ARCH" = "x86" ]; then
   ui_print "- Placing x86 libraries"
 
   rm -rf "$MODPATH/zygisk/armeabi-v7a.so" "$MODPATH/zygisk/arm64-v8a.so" "$MODPATH/zygisk/x86_64.so"
   rm -rf "$MODPATH/lib/armeabi-v7a" "$MODPATH/lib/arm64-v8a" "$MODPATH/lib/x86_64"
 fi
 
-if [ "$ARCH" = "x64" ] then
+if [ "$ARCH" = "x64" ]; then
   ui_print "- Placing x86_64 libraries"
 
   rm -rf "$MODPATH/zygisk/armeabi-v7a.so" "$MODPATH/zygisk/arm64-v8a.so"
